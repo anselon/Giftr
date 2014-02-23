8a74b30ea35d8c00cd6a78b7a7912eec50678a11
anselon <abselonick@Anna-Selonicks-MacBook-Pro-2.local>
2014-02-23 15:25:21 -0600

    gift logo
    added gift logo with link to home

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php 
 /** Homepage Template **/
 ?>
+
+<html>
+<head>
+</head>
+<body>
+	<div class ="toolbar" id="gift">
+	<a href ="index.php">
+	<img src="image/Gift.png" alt="giftrlogo" height="42" width="42"/>
+	</a>
+	</div>
+
+</body>
+
+
+</html>
